6987959de296ae6434cfdaf4c066a738f4225c98
MarioMottl <58133541+MarioMottl@users.noreply.github.com>
2019-12-03 13:09:19 +0100

    Add files via upload
    Added a 50ms sleep timer for the blinking. Futher improvements with the variable timer coming soon

--- Verkehrsampel.s
+++ Verkehrsampel.s
@@ -71,15 +71,15 @@ phase9ret	; Ausgeben
 			END
 
 ;******************************************************************************
-;*            U N T E R P R O G R A M M:    wait_500ms                        *
+;*            U N T E R P R O G R A M M:    wait_50ms                        *
 ;*                                                                            *
-;* Aufgabe:   Wartet 500ms                                                    *
+;* Aufgabe:   Wartet 50ms                                                    *
 ;* Input:                                                                     *
 ;* return:	 	                                                              *
 ;******************************************************************************
-wait_500ms		PROC
+wait_50ms		PROC
 				push 	{R0-R2,LR}	   ; save link register to Stack
-                MOV     R0,#0x1F4	   ; wait 500ms
+                MOV     R0,#0x32	   ; wait 50ms
                 MOV     R1,#0
 wait_ms_loop	MOV		R2,#0x63B			
 wait_ms_loop1	SUB 	R2,R2,#1
@@ -90,9 +90,7 @@ wait_ms_loop1	SUB 	R2,R2,#1
 				BNE		wait_ms_loop
 				POP 	{R0-R2,PC}	   ;restore link register to Programm Counter and return
 				ENDP
-				
 				ALIGN
-
 				END
 
 ;*************************************************************************
@@ -103,11 +101,12 @@ wait_ms_loop1	SUB 	R2,R2,#1
 ;* return: keine *
 ;*************************************************************************
 
-waitvar	;
-		PUSH {LR}
-
-		POP {PC}
-		END
+waitvar			PROC;
+				PUSH {R0,R1,LR}
+				LDR R0,=GPIOA_IDR
+				AND 
+				POP {R0,R1,PC}
+				END
 
 ;*************************************************************************
 ;* U N T E R P R O G R A M M: init_ports *
